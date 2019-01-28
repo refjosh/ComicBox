ade251f1a3e61659dc2a5d36da5ea68a50d6036e
Joshua Okoro <joshuaojc@gmail.com>
2019-01-27 18:36:56 -0600

    Initial Commit

---
+++ ComicBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComicBox
+//
+//  Created by Joshua Okoro on 1/27/19.
+//  Copyright © 2019 Joshua Okoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
