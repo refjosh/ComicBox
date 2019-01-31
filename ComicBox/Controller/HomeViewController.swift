6197744eb4e66989c0cdd6b4845401ee8aa7e36e
Joshua Okoro <joshuaojc@gmail.com>
2019-01-31 15:39:09 -0600

    Recent Release updates with data from api

--- ComicBox/Controller/HomeViewController.swift
+++ ComicBox/Controller/HomeViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import ProgressHUD
 
 class HomeViewController: UIViewController {
+    
     
     //MARK: - Recent Release UI Elements
     @IBOutlet weak var recentReleaseDate: UILabel!
@@ -24,8 +28,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        ProgressHUD.show("Updating")
+        getRecentRelease()
     }
     
     //MARK: - View All Comics Button Pressed
@@ -38,15 +42,49 @@ class HomeViewController: UIViewController {
     
     
     //MARK: - Network To Get Content
+    func getRecentRelease() {
+        Alamofire.request("https://xkcd.com/info.0.json").validate().responseJSON { (response) in
+            switch response.result {
+            case .success:
+                ProgressHUD.showSuccess("Got New Release!")
+                let resultInJSON = JSON(response.result.value!)
+                self.updateWithRecentRelease(resultInJSON)
+                
+            case .failure(let error):
+                ProgressHUD.showError("Unable To Get New Release\n😞")
+                self.handleErrors(error)
+            }
+        }
+    }
     
     
-    //MARK: - Unwrap with JSON
-    
+    //MARK: - Unwrap with JSON and Update Recent Release View
+    func updateWithRecentRelease(_ result: JSON) {
+        let year = result["year"].string!
+        let month = result["month"].string!
+        let day = result["day"].string!
+        
+        recentReleaseTitle.text = result["safe_title"].string
+        recentReleaseDate.text = "\(month)-\(day)-\(year)"
+        recentReleaseDescription.text = result["alt"].string
+    }
     
     //MARK: - Display Content To Recent Release View
     
+    func handleErrors(_ error: Error) {
+        print(error)
+    }
+    
     
     //MARK: - Display Content to Comic Shelf View
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeComicShelfCell", for: indexPath)
+//        return cell
+//    }
     
 
 }
